c3dfa6d1554a7657639e124388338f6f6827801f
Bruno Santos <brunorjcf@gmail.com>
2020-02-19 09:58:05 -0300

    findByDepartment Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,6 @@
 package application;
 
-import java.util.Date;
+import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,13 +11,24 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Books");
+SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		Seller seller = new Seller(33, "Bruno Santos", "Brunorjcf@gmail.com", new Date(), 7000.0, obj);
-		
-		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
+		System.out.println("=== TEST 1: seller findById =====");
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
-	}
+		
+		System.out.println("\n=== TEST 2: seller findByDepartment =====");
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 
+		System.out.println("\n=== TEST 3: seller findAll =====");
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+
+	}
 }
